e8eb719637c6ac23eb780f8889e9050eadca496f
InGwonKang <tnqkr3494@naver.com>
2024-02-24 14:18:24 +0900

    Hydration_useclient

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Tomato() {
-  return <h1>Hello!</h1>;
+  return <h1>Hello</h1>;
 }
---
+++ components/navigation.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Nav() {
+  const path = usePathname();
+  console.log(path);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about-us">About us</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
