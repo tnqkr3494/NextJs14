6e7cb741d8fcd5f3d755478166a35a494e68af90
InGwonKang <tnqkr3494@naver.com>
2024-02-25 18:15:38 +0900

    Nextjs14_SameTime

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
@@ -5,9 +7,6 @@ export const metadata = {
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  console.log("getMovies start");
-
-  await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(API_URL);
   const json = response.json();
   return json;
@@ -15,5 +14,13 @@ async function getMovies() {
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <h6>{JSON.stringify(movies)}</h6>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
