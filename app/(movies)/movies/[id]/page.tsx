a4d39481fb742022f1bf4cbd6d879cb13af7f8a9
InGwonKang <tnqkr3494@naver.com>
2024-02-26 12:14:23 +0900

    movie_videos

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -8,10 +8,11 @@ export default async function MovieDetail({
   params: { id: string };
 }) {
   return (
-    <div>
+    <div style={{ width: "95%", margin: "0 auto" }}>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
+      <hr />
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,6 +1,20 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
-async function getVideos(id: string) {
+interface IVideos {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
+async function getVideos(id: string): Promise<IVideos[]> {
   console.log(`Fetching videos: ${Date.now()}`);
   //await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(`${API_URL}/${id}/videos`);
@@ -8,6 +22,21 @@ async function getVideos(id: string) {
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
-  const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  const videos: IVideos[] = await getVideos(id);
+  return (
+    <div className={styles.wrapper}>
+      <p>Related Videos</p>
+      <div className={styles.container}>
+        {videos.map((video) => (
+          <iframe
+            key={video.id}
+            src={`https://youtube.com/embed/${video.key}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title={video.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
